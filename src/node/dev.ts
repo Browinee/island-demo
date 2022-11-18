47f19564ab612cded280afba3c21951c35ee9c67
User <user@AdmindeMacBook-Air.local>
2022-11-18 11:25:45 +0800

    feat: add html template and hmr

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,7 +1,9 @@
 import { createServer as createViteDevServer } from "vite";
-
+import { pluginIndexHtml } from "./plugin-island/indexHtml";
+import pluginReact from "@vitejs/plugin-react";
 export async function createDevServer(root = process.cwd()) {
   return createViteDevServer({
     root,
+    plugins: [pluginIndexHtml(), pluginReact()],
   });
 }
