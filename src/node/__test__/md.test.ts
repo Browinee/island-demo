5fa29c1a64e7a0e22649a17346ef63dac05fe512
Browinee <nihilitypeo@gmail.com>
2023-04-27 19:00:08 +0800

    feat: add rehype plugin preWrapper

--- src/node/__test__/md.test.ts
+++ src/node/__test__/md.test.ts
@@ -3,12 +3,14 @@ import { describe, test, expect } from "vitest";
 import remarkParse from "remark-parse";
 import remarkRehype from "remark-rehype";
 import rehypeStringify from "rehype-stringify";
+import { rehypePluginPreWrapper } from "../plugin-mdx/rehypePlugins/preWrapper";
 
 describe("Markdown compile cases", () => {
   const processor = unified()
     .use(remarkParse)
     .use(remarkRehype)
-    .use(rehypeStringify);
+    .use(rehypeStringify)
+    .use(rehypePluginPreWrapper);
 
   test("Compile title", async () => {
     const mdContent = "# 123";
@@ -19,6 +21,16 @@ describe("Markdown compile cases", () => {
   test("Compile code", async () => {
     const mdContent = "I am using `Island.js`";
     const result = processor.processSync(mdContent);
-    expect(result.value).toMatchInlineSnapshot('"<p>I am using <code>Island.js</code></p>"');
+    expect(result.value).toMatchInlineSnapshot(
+      '"<p>I am using <code>Island.js</code></p>"',
+    );
+  });
+  test.only("Compile code block", async () => {
+    const mdContent = "```js\nconsole.log(123);\n```";
+    const result = processor.processSync(mdContent);
+    expect(result.value).toMatchInlineSnapshot(`
+      "<div class=\\"language-js\\"><span class=\\"lang\\">js</span><pre><code class=\\"\\">console.log(123);
+      </code></pre></div>"
+    `);
   });
 });
--- src/runtime/App.tsx
+++ src/runtime/App.tsx
@@ -3,3 +3,5 @@ import { Layout } from "../theme-default";
 export function App() {
   return <Layout />;
 }
+
+App();
